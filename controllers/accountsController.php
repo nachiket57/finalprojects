6ec608151859cc99ba195cab35121a326b06c15d
Nachiket M Desai <nmd33@afsconnect1.njit.edu>
2017-12-17 13:02:30 -0500

    changes in controller

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kwilliams
- * Date: 11/27/17
- * Time: 5:32 PM
- */
+
 
 
 //each page extends controller and the index.php?page=tasks causes the controller to be called
@@ -17,19 +12,19 @@ class accountsController extends http\controller
     {
         $record = accounts::findOne($_REQUEST['id']);
         self::getTemplate('show_account', $record);
+
     }
 
-    //to call the show function the url is index.php?page=accounts&action=all
+    //to call the show function the url is index.php?page=task&action=list_task
 
     public static function all()
     {
-
-        $records = accounts::findAll();
-        self::getTemplate('all_accounts', $records);
+       // $records = accounts::findAll();
+       // self::getTemplate('all_accounts', $records);
 
     }
     //to call the show function the url is called with a post to: index.php?page=task&action=create
-    //this is a function to create new tasks
+    //this is a function to create ne,w tasks
 
     //you should check the notes on the project posted in moodle for how to use active record here
 
@@ -38,45 +33,45 @@ class accountsController extends http\controller
     {
         //https://www.sitepoint.com/why-you-should-use-bcrypt-to-hash-stored-passwords/
         //USE THE ABOVE TO SEE HOW TO USE Bcrypt
+       // print_r($_POST);
+
         self::getTemplate('register');
+        //this just shows creating an account.
+
     }
 
-    //this is the function to save the user the new user for registration
+    //this is the function to save the user the user profile
     public static function store()
-
     {
-        $user = accounts::findUserbyEmail($_REQUEST['email']);
+      //  print_r($_POST);
+        $user = accounts::findUserbyEmail($_POST['email']);
+
+        if($user==FALSE) {
+
+            session_start();
+            $record = new account();
+           // print_r($record);
+            $record->birthday = $_POST['birthday'];
+            $record->gender = $_POST['gender'];
+            $record->email = $_POST['email'];
+            $record->fname = $_POST['fname'];
+            $record->lname = $_POST['lname'];
+            $record->phone = $_POST['phone'];
+            $record->password = utility\password::setPassword($_POST['password']);
+            $record->save();
+            $user = accounts::findUserbyEmail($_POST['email']);
 
 
-        if ($user == FALSE) {
-            $user = new account();
-            $user->email = $_POST['email'];
-            $user->fname = $_POST['fname'];
-            $user->lname = $_POST['lname'];
-            $user->phone = $_POST['phone'];
-            $user->birthday = $_POST['birthday'];
-            $user->gender = $_POST['gender'];
-            //$user->password = $_POST['password'];
-            //this creates the password
-            //this is a mistake you can fix...
-            //Turn the set password function into a static method on a utility class.
-            $user->password = $user->setPassword($_POST['password']);
-            $user->save();
+            $_SESSION["email"] = $user->email;
+            $_SESSION["userID"] = $user->id;
 
-            //you may want to send the person to a
-            // login page or create a session and log them in
-            // and then send them to the task list page and a link to create tasks
-            header("Location: index.php?page=accounts&action=all");
-
-        } else {
-            //You can make a template for errors called error.php
-            // and load the template here with the error you want to show.
-           // echo 'already registered';
+           header("Location:https://web.njit.edu/~nmd33/mvc/index.php?page=tasks&action=all");
+        }
+        else
+        {
             $error = 'already registered';
             self::getTemplate('error', $error);
-
         }
-
     }
 
     public static function edit()
@@ -85,27 +80,6 @@ class accountsController extends http\controller
 
         self::getTemplate('edit_account', $record);
 
-    }
-//this is used to save the update form data
-    public static function save() {
-        $user = accounts::findOne($_REQUEST['id']);
-
-        $user->email = $_POST['email'];
-        $user->fname = $_POST['fname'];
-        $user->lname = $_POST['lname'];
-        $user->phone = $_POST['phone'];
-        $user->birthday = $_POST['birthday'];
-        $user->gender = $_POST['gender'];
-        $user->save();
-        header("Location: index.php?page=accounts&action=all");
-
-    }
-
-    public static function delete() {
-
-        $record = accounts::findOne($_REQUEST['id']);
-        $record->delete();
-        header("Location: index.php?page=accounts&action=all");
     }
 
     //this is to login, here is where you find the account and allow login or deny.
@@ -116,33 +90,104 @@ class accountsController extends http\controller
         //then you need to check the password and create the session if the password matches.
         //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
         //after you login you can use the header function to forward the user to a page that displays their tasks.
-        //        $record = accounts::findUser($_POST['email']);
+        //        $record = accounts::findUser($_POST['uname']);
+
+          //  print_r($_REQUEST['email']);
 
         $user = accounts::findUserbyEmail($_REQUEST['email']);
 
-
-        if ($user == FALSE) {
-            echo 'user not found';
-        } else {
-
-            if($user->checkPassword($_POST['password']) == TRUE) {
-
-                echo 'login';
-
-                session_start();
-                $_SESSION["userID"] = $user->id;
-
-                //forward the user to the show all todos page
-                print_r($_SESSION);
-            } else {
-                echo 'password does not match';
-            }
+        if($user==FALSE)
+        {
+            $error = 'user not found';
+            self::getTemplate('error', $error);
 
         }
+        else
+        {
+
+          if($user->checkPassword($_POST['password'])== TRUE)
+             {
+               // echo 'login';
+
+                session_start();
+
+                $_SESSION["userID"] = $user->id;
+                $_SESSION["email"] = $user->email;
 
 
+                 header("location: https://web.njit.edu/~nmd33/mvc/index.php?page=tasks&action=all");
+
+
+             }
+
+             else
+             {
+
+                 $error = 'password does not match';
+                 self::getTemplate('error', $error);
+             }
+
+               }
+
+    }
+
+    public static function delete()
+    {
+      $record = accounts::findOne($_REQUEST['id']);
+      $record->delete();
+      header("location: https://web.njit.edu/~nmd33/mvc/index.php");
+    }
+
+    static  public function test()
+    {
+        if ($_POST['btSubmit']=='Edit') {
+
+        accountsController::edit();
+
+        }
+         elseif ($_POST['btSubmit']=='Delete') {
+
+        accountsController::delete();
+        }
+
+    }
+
+    static public function update()
+    {
+        session_start();
+
+        $record = new account();
+        $record->email = $_POST['email'];
+        $record->fname = $_POST['fname'];
+        $record->lname = $_POST['lname'];
+        $record->phone = $_POST['phone'];
+        $record->birthday = $_POST['birthday'];
+        $record->gender = $_POST['gender'];
+        $record->password = $_POST['password'];
+        $record->id = $_SESSION['userID'];
+        $record->save();
+
+        header("location: https://web.njit.edu/~nmd33/mvc/index.php?page=tasks&action=all");
 
 
     }
+
+    static public function profile()
+    {
+        session_start();
+        $record = accounts::findUserbyEmail($_SESSION['email']);
+
+        self::getTemplate('edit_account', $record);
+
+
+    }
+
+
+
+
+
+
+
+
 
 }
