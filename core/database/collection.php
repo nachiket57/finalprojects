93ed41cdea1335549195aa591aabfd80ded3abe4
nachiket57 <31973100+nachiket57@users.noreply.github.com>
2017-12-17 21:44:29 -0500

    Update collection.php

--- core/database/collection.php
+++ core/database/collection.php
@@ -2,10 +2,11 @@
 
 namespace database;
 
+
 abstract class collection
 {
 
-    //factory to make model
+    //factory to  make model
     static public function create()
     {
         $model = new static::$modelName;
